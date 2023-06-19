7490fdd023f5dec8c816f3d92ee3118b2271bada
gorogoroumaru <zokutyou2@gmail.com>
2023-06-19 17:12:24 +0900

    change description of login button after login

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,5 @@
 <script>
 	import '../app.postcss';
-	import BottomNavigation from '../components/BottomNavigation.svelte';
 
 	import { goto } from '$app/navigation';
 	import { invalidate } from '$app/navigation';
@@ -10,6 +9,10 @@
 
 	let { supabase, session } = data;
 	$: ({ supabase, session } = data);
+
+	const user_id = data.session?.user.id;
+
+	// TODO login時、ログインボタンを投稿ボタンに変更する
 
 	onMount(() => {
 		const {
@@ -24,40 +27,18 @@
 	});
 </script>
 
-<!-- BottomNavigationをなくす　-->
+<!-- TODO footerを作成　-->
 
 <div class="AppHeader_inner__Nqogt">
-	<a class="AppHeader_homeLink__sgij0" href="/"
-		><svg
-			x="0px"
-			y="0px"
-			viewBox="0 0 377.4 88.3"
-			aria-label="Zenn | エンジニアのための情報共有コミュニティ"
-			height="22"
-		>
-			<title>Zenn</title>
-			<g fill="#111">
-				<path
-					d="M233,56.8h-39c0.5,3.5,2.2,6.8,4.8,9.2c2.7,2.3,6.2,3.5,9.8,3.4c2.8,0,5.6-0.5,8.2-1.7c2.5-1.1,4.8-2.8,6.5-5l8.2,9.5 c-2.5,3.4-5.7,6.1-9.5,7.9c-4.6,2.2-9.6,3.3-14.7,3.2c-5.7,0.1-11.4-1.2-16.5-4c-4.5-2.5-8.2-6.3-10.7-10.9s-3.8-9.8-3.7-15.1v-2.2 c-0.1-5.7,1.1-11.3,3.5-16.5c2.2-4.7,5.7-8.6,10.1-11.3c4.7-2.8,10.1-4.2,15.5-4.1c5.2-0.1,10.3,1.1,14.9,3.7 c4.1,2.5,7.4,6.2,9.4,10.5c2.2,5.1,3.3,10.5,3.2,16.1V56.8z M216.1,43.9c0.1-2.9-0.9-5.7-2.8-7.9c-1.8-1.9-4.4-2.9-7.9-2.9 c-2.9-0.1-5.8,1.1-7.7,3.2c-2,2.6-3.3,5.7-3.6,9h22V43.9z"
-				/>
-				<path d="M128.3,67.9h36.1v14.7h-56.9V72l35.8-54.3h-36.2V2.9h56.6v10.4L128.3,67.9z" />
-				<path
-					d="M248.8,50.7c0-19.1,12.7-29.2,28.2-29.2s27.9,10.1,27.9,29.2V82h-16V51.4c0-10.6-4.8-16.1-12-16.1s-12.4,5.5-12.4,16.1 v30.7h-15.8L248.8,50.7L248.8,50.7z"
-				/>
-				<path
-					d="M320.3,50.7c0-19.1,12.7-29.2,28.2-29.2s27.9,10.1,27.9,29.2V82h-16V51.4c0-10.6-4.8-16.1-12-16.1S336,40.8,336,51.4v30.7 h-15.8L320.3,50.7L320.3,50.7z"
-				/>
-			</g>
+	<a class="AppHeader_homeLink__sgij0" href="/">
+		<svg viewBox="0 0 27 27" height="23" width="23">
 			<path
-				fill="#3EA8FF"
-				class="st0"
-				d="M2.4,83.3h17c0.9,0,1.7-0.5,2.2-1.2L68.4,5.2C69,4.2,68.3,3,67.1,3H51c-0.8,0-1.5,0.4-1.9,1.1L1.6,81.9 C1.3,82.5,1.7,83.3,2.4,83.3z"
-			/>
-			<path
-				fill="#3EA8FF"
-				class="st0"
-				d="M61,82.1l22.1-35.5c0.7-1.1-0.1-2.5-1.4-2.5H65.7c-0.6,0-1.2,0.3-1.5,0.8L41.5,81.2c-0.6,0.9,0.1,2.1,1.2,2.1 h16.3C59.8,83.3,60.6,82.9,61,82.1z"
+				d="M13.13 22.19L11.5 18.36C13.07 17.78 14.54 17 15.9 16.09L13.13 22.19M5.64 12.5L1.81 10.87L7.91 8.1C7 9.46 6.22 10.93 5.64 12.5M21.61 2.39C21.61 2.39 16.66 .269 11 5.93C8.81 8.12 7.5 10.53 6.65 12.64C6.37 13.39 6.56 14.21 7.11 14.77L9.24 16.89C9.79 17.45 10.61 17.63 11.36 17.35C13.5 16.53 15.88 15.19 18.07 13C23.73 7.34 21.61 2.39 21.61 2.39M14.54 9.46C13.76 8.68 13.76 7.41 14.54 6.63S16.59 5.85 17.37 6.63C18.14 7.41 18.15 8.68 17.37 9.46C16.59 10.24 15.32 10.24 14.54 9.46M8.88 16.53L7.47 15.12L8.88 16.53M6.24 22L9.88 18.36C9.54 18.27 9.21 18.12 8.91 17.91L4.83 22H6.24M2 22H3.41L8.18 17.24L6.76 15.83L2 20.59V22M2 19.17L6.09 15.09C5.88 14.79 5.73 14.47 5.64 14.12L2 17.76V19.17Z"
+				fill="currentColor"
 			/></svg
+		><span
+			class="self-center whitespace-nowrap text-2xl font-semibold text-gray-900 dark:text-white"
+			>Study Frontier</span
 		></a
 	>
 	<div class="FadeIn_fadeIn__O8JSZ">
@@ -70,13 +51,18 @@
 					/></svg
 				></a
 			>
-			<button on:click={() => goto('/login')} class="AppHeader_signInLink__K1JEF">Log in</button>
+			<button
+				on:click={() => {
+					if (!user_id) goto('/login');
+					goto('/register');
+				}}
+				class="AppHeader_signInLink__K1JEF">{user_id ? '投稿する' : 'ログイン'}</button
+			>
 		</div>
 	</div>
 </div>
 
 <slot />
-<BottomNavigation />
 
 <style>
 	.AppHeader_inner__Nqogt {
