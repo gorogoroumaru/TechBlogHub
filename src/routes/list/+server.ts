ac52bed1188df3f659ceff9b3d20229d5d3068e2
gorogoroumaru <zokutyou2@gmail.com>
2023-06-29 08:56:25 +0900

    add pagination 2

--- src/routes/list/+server.ts
+++ src/routes/list/+server.ts
@@ -3,6 +3,7 @@ import { json } from '@sveltejs/kit';
 
 export async function GET({ url }) {
 	const tag = url.searchParams.get('tag') ?? '';
-	const result = await getResources(tag);
+	const page = parseInt(url.searchParams.get('page') ?? '0');
+	const result = await getResources(tag, page);
 	return json({ rows: result.rows });
 }
