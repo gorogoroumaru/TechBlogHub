f334e6a415f6f4dd3158cfe989fc9c84db70e1f0
gorogoroumaru <zokutyou2@gmail.com>
2023-06-29 09:42:50 +0900

    remove comment and console.log

--- src/routes/detail/[slug]/+page.server.ts
+++ src/routes/detail/[slug]/+page.server.ts
@@ -1,10 +1,8 @@
 import { getResourceById } from '../../../repository/resource.server';
 
 export async function load({ params }) {
-	// TODO idから記事を取得
 	const id = params.slug;
 	const resource = await getResourceById(id);
-	console.log(resource.rows[0]);
 	return {
 		...resource.rows[0]
 	};
