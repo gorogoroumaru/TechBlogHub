240cfc2dfa4d251f6fd45eb9924ddb932432d6f2
gorogoroumaru <zokutyou2@gmail.com>
2023-07-11 20:53:26 +0900

    fix tailwind configuration

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,17 +1,23 @@
-/** @type {import('tailwindcss').Config}*/
 const config = {
 	content: [
-		"./src/**/*.{html,js,svelte,ts}",
-		"./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}",
+	  "./src/**/*.{html,js,svelte,ts}",
+	  "./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}",
 	],
 
 	plugins: [
-		require('flowbite/plugin')
+	  require('flowbite/plugin')
 	],
 
-	theme: {
-		extend: {}
-	}
-};
+	darkMode: 'class',
 
-module.exports = config;
+	theme: {
+	  extend: {
+		colors: {
+		  // flowbite-svelte
+		  primary: { 50: '#FFF5F2', 100: '#FFF1EE', 200: '#FFE4DE', 300: '#FFD5CC', 400: '#FFBCAD', 500: '#FE795D', 600: '#EF562F', 700: '#EB4F27', 800: '#CC4522', 900: '#A5371B'},
+		}
+	  }
+	}
+  };
+
+  module.exports = config;
