f4cd7d9ea04492198c5c3af9aee7afde49ac71e6
gorogoroumaru <zokutyou2@gmail.com>
2023-07-31 21:26:31 +0900

    fix bug in fulltext search in searchbar

--- src/repository/resource.server.ts
+++ src/repository/resource.server.ts
@@ -22,7 +22,7 @@ export async function getResourceByTag(tag: string, page: number) {
 
 export async function getResourceByKeyword(keyword: string, page: number) {
 	const result = await conn.execute(
-		'select rs.id, title, description, url, image_url, user_id, created_at, updated_at, group_concat(t.tag_name) as tag_name from Resources as rs INNER JOIN Tags as t ON rs.id = t.resource_id where match(`title`, `description`) against(?) group by rs.id limit 10 offset ?',
+		'select rs.id, title, description, url, image_url, user_id, created_at, updated_at, group_concat(t.tag_name) as tag_name from Resources as rs INNER JOIN Tags as t ON rs.id = t.resource_id where match(`title`, `description`) against(? in natural language mode) group by rs.id limit 10 offset ?',
 		[keyword, page * 10]
 	);
 	return result?.rows;
