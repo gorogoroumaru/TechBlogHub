886e8711a86302d7e122f74ecd03a60285c519f6
gorogoroumaru <zokutyou2@gmail.com>
2023-08-06 16:14:13 +0900

    create app.d.ts for supabase type definition

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,12 +1,14 @@
-// See https://kit.svelte.dev/docs/types#app
-// for information about these interfaces
+import { SupabaseClient, Session } from '@supabase/supabase-js';
+import type { Database } from './types';
+
 declare global {
 	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface Platform {}
+		interface Locals {
+			supabase: SupabaseClient<Database>;
+			getSession(): Promise<Session | null>;
+		}
+		interface PageData {
+			session: Session | null;
+		}
 	}
 }
-
-export {};
