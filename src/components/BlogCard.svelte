e4b9e2f9c82e925483d300b47159d6d821b4d768
gorogoroumaru <zokutyou2@gmail.com>
2023-08-08 21:57:31 +0900

    changed blogcard component to specify size of the image

--- src/components/BlogCard.svelte
+++ src/components/BlogCard.svelte
@@ -1,29 +1,35 @@
 <script lang="ts">
-	import { Badge, Card } from 'flowbite-svelte';
+	import { Badge } from 'flowbite-svelte';
 	import type { Resource } from '../types/resource';
 	import placeholderImg from '../placeholder.png';
 	export let resource: Resource;
 </script>
 
-<Card
-	img={resource?.image_url ?? placeholderImg}
+<a
 	href="/detail/{resource?.id}"
-	horizontal={false}
-	reverse={false}
-	class="m-2"
+	class="m-2 max-w-sm bg-white border border-gray-200 rounded-lg shadow md:flex-row lg:max-w-lg hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
 >
-	<h5 class="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-		{resource?.title}
-	</h5>
-	<p class="mt-4 text-xs text-gray-900 dark:text-white">
-		{resource.created_at}
-	</p>
-	<div>
-		{#each resource.tag_name.split(',') as tag}
-			<Badge class="mt-2 mb-4 mr-1">{tag}</Badge>
-		{/each}
+	<img
+		class="object-cover w-full rounded-t-lg md:rounded-none md:rounded-l-lg"
+		src={resource?.image_url ?? placeholderImg}
+		width={200}
+		height={100}
+		alt=""
+	/>
+	<div class="flex flex-col justify-between p-4 leading-normal">
+		<h3 class="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+			{resource?.title}
+		</h3>
+		<p class="mt-4 text-xs text-gray-900 dark:text-white">
+			{resource.created_at}
+		</p>
+		<div>
+			{#each resource.tag_name.split(',') as tag}
+				<Badge class="mt-2 mb-4 mr-1">{tag}</Badge>
+			{/each}
+		</div>
+		<p class="font-normal text-gray-700 dark:text-gray-400 leading-tight truncate">
+			{resource?.description}
+		</p>
 	</div>
-	<p class="font-normal text-gray-700 dark:text-gray-400 leading-tight truncate">
-		{resource?.description}
-	</p>
-</Card>
+</a>
