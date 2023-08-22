b976bd5afebdc2739c004ebef754930833c8493f
gorogoroumaru <zokutyou2@gmail.com>
2023-08-22 10:38:03 +0900

    create terms of use and privacy policy

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -71,9 +71,7 @@
 	<FooterLinkGroup
 		ulClass="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0"
 	>
-		<FooterLink href="/">About</FooterLink>
-		<FooterLink href="/">Privacy Policy</FooterLink>
-		<FooterLink href="/">Licensing</FooterLink>
-		<FooterLink href="/">Contact</FooterLink>
+		<FooterLink href="/termsOfUse">Terms of Use</FooterLink>
+		<FooterLink href="/privacyPolicy">Privacy Policy</FooterLink>
 	</FooterLinkGroup>
 </Footer>
