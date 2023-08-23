a149d6505ad7b00456d05c66913762358b9aaf05
gorogoroumaru <zokutyou2@gmail.com>
2023-08-23 21:59:14 +0900

    change style of blogcard for responsive layout

--- src/components/BlogCard.svelte
+++ src/components/BlogCard.svelte
@@ -7,19 +7,18 @@
 </script>
 
 <a
-	class="w-full lg:max-w-full flex mb-8 shadow-sm transition hover:shadow-lg"
+	class="w-full lg:max-w-full md:flex mb-8 shadow-sm transition hover:shadow-lg"
 	href="/detail/{resource?.id}"
 >
 	<!-- svelte-ignore a11y-missing-attribute -->
 	<img
-		class="object-cover rounded-t-lg md:rounded-none md:rounded-l-lg"
+		class="object-cover rounded-t-lg md:rounded-none md:rounded-l-lg md:w-1/3"
 		src={image_url}
-		on:error={(event) => (
-			event.target.src = placeholderImg
+		on:error={(event) => {
+			event.target.src = placeholderImg;
 			event.onerror = null;
-			)}
+		}}
 		height="100%"
-		width="30%"
 	/>
 	<div
 		class="w-full border-r border-b border-l border-gray-100 lg:border-t bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal"
