b8319b95dddbcd2feca53abf10c28bceb88ac78e
gorogoroumaru <zokutyou2@gmail.com>
2023-08-26 22:00:22 +0900

    remove error handling from registerTags

--- src/repository/tag.server.ts
+++ src/repository/tag.server.ts
@@ -2,15 +2,10 @@ import { conn } from './dbconnect.server';
 import type { Tags } from '../types/tags';
 
 export async function registerTags(tags: Tags) {
-	try {
-		for (const tag of tags.tags) {
-			await conn.execute('insert into Tags (resource_id, tag_name) values (?, ?)', [
-				tags.resource_id,
-				tag
-			]);
-		}
-		return true;
-	} catch (e) {
-		return false;
+	for (const tag of tags.tags) {
+		await conn.execute('insert into Tags (resource_id, tag_name) values (?, ?)', [
+			tags.resource_id,
+			tag
+		]);
 	}
 }
