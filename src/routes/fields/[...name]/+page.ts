3decd945f965e30745c6f6858599d87aa4643be9
gorogoroumaru <zokutyou2@gmail.com>
2023-09-08 22:35:06 +0900

    change logic to fetch subField

--- src/routes/fields/[...name]/+page.ts
+++ src/routes/fields/[...name]/+page.ts
@@ -1,16 +1,12 @@
-export const load = async ({ fetch, params, data }) => {
-	let tags: string[] = [];
+import { subField } from '../../../data/fields.js';
+
+export const load = async ({ params, data }) => {
 	const field = params.name
 		.split('/')
 		.filter((field) => field != '')
 		.slice(-1)[0];
 
-	try {
-		const res = await fetch(`/json/${field}.tags.json`);
-		tags = await res.json();
-	} catch (err) {
-		console.log(`json not found for tag ${field}`);
-	}
+	const tags = subField?.[field]?.items || [];
 
 	return {
 		tags,
